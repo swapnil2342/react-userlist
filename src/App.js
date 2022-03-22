4450c84eb76a263919fcae6785202085b00b1470
swapnilgandal8 <swapnilgandal8@gmail.com>
2022-03-22 15:24:01 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Drawer from "@mui/material/Drawer";
+import { createPortal } from "react-dom";
+import Profile from "./Profile/profile";
+import Post from "./Post/post";
+import UserList from "./UserList/UserList";
+import RightMenu from "./RightMenu/RightMenu";
 
 function App() {
+  const [userList, setUserList] = useState([]);
+  const [user, setUser] = useState({});
+  const [userID, setUserID] = useState("");
+  const [openProfile, setOpenProfile] = useState(false);
+  const [openPost, setOpenPost] = useState(false);
+  const [selectedTab, setSelectedTab] = useState("");
+  const [contentRef, setContentRef] = useState(null);
+  const mountNode = contentRef?.contentWindow?.document?.body;
+  let rows = [];
+  const headNode = contentRef?.contentWindow?.document?.head;
+
+  useEffect(() => {
+    if (headNode) {
+      headNode.innerHTML = document?.head?.innerHTML;
+    }
+  }, [headNode]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((result) => {
+        result.map((obj) => {
+          rows.push(obj);
+        });
+        setUserList(rows);
+      });
+  }, []);
+
+  const getSelectedUser = (userID) => {
+    setUser(userList[userID]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="App">
+        <iframe width={"100%"} ref={setContentRef}>
+          {mountNode &&
+            headNode &&
+            createPortal(
+              <UserList
+                userList={userList}
+                getSelectedUser={getSelectedUser}
+                setOpenPost={setOpenPost}
+                setOpenProfile={setOpenProfile}
+                setSelectedTab={setSelectedTab}
+                setUser={setUser}
+                setUserID={setUserID}
+              />,
+              mountNode
+            )}
+        </iframe>
+
+        <RightMenu selectedTab={selectedTab} />
+      </div>
+      {user && (
+        <Drawer
+          anchor={"right"}
+          open={openProfile}
+          onClose={() => {
+            setOpenProfile(false);
+            setSelectedTab("");
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Profile user={user} />
+        </Drawer>
+      )}
+      {userID && (
+        <Drawer
+          anchor={"right"}
+          open={openPost}
+          onClose={() => {
+            setOpenPost(false);
+            setSelectedTab("");
+          }}
+        >
+          <Post userID={userID} userList={userList} />
+        </Drawer>
+      )}
+    </>
   );
 }
 
